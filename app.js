c0c462e74bbe743d9ef7eff63c480605aa46991a
Maxime liege <liege30@gmail.com>
2018-10-11 13:55:07 +0200

    add logger and prettier

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'debug',
+  format: format.simple(),
+  transports: [new transports.Console()]
+});
+
+
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
 	res.send(200, 'Simple route work!');
@@ -8,5 +20,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, () => {
-	console.log(`Server running on port: ${port}`);
+	logger.info(`Server running on port: ${port}`);
 });
