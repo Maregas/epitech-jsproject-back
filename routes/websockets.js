14cd231837ee367cfec07b8deda32441a72e7ce5
Mathéo MSA <matheo.msa@epitech.eu>
2018-12-02 16:53:31 +0100

    Username server

--- routes/websockets.js
+++ routes/websockets.js
@@ -8,8 +8,8 @@ chatroom.on('connection', socket => {
   socket.on('room', data => {
     logger.info(`user ${data.username} join ${data.room}`);
     socket.join(data.room);
-    socket.emit('connectionMessage', `Welcome on the ${data.room} room.`);
-    chatroom.in(data.room).emit('connectionMessage', `user ${data.username} join ${data.room}`)
+    socket.emit('connectionMessage', {username: "Server", msg: `Welcome on the ${data.room} room.`});
+    chatroom.in(data.room).emit('connectionMessage', {username: "Server", msg: `user ${data.username} join ${data.room}`}):
   });
 
   socket.on('chat message', data => {
